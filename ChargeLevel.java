0c4f599505bb442cdc2304234ea534175e5c16db
Vidit Khandelwal <63170797+csvidit@users.noreply.github.com>
2020-11-15 02:07:06 +0530

    possible OOP implementation for the high-level part

---
+++ ChargeLevel.java
@@ -0,0 +1,15 @@
+public class ChargeLevel{
+    
+    private short chargeLevel;
+
+    ChargeLevel()
+    {
+        chargeLevel=Short.MAX_VALUE;
+    }
+
+    public double decode()
+    {
+        //to be done
+    }
+
+}
