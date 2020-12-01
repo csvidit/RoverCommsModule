cf3474fcd1f8a98830a44575a63e799ef5c89418
Wesley Hershberger <mggmugginsmc@gmail.com>
2020-12-01 10:20:25 -0500

    Add binary output to python script

--- MissionControl/High_Level_Design/log_scale.py
+++ MissionControl/High_Level_Design/log_scale.py
@@ -4,14 +4,20 @@ def encode(x):
             return (exponent, round(x / 2**exponent))
     raise Exception("x was too big!")
 
+def encoded_as_int(encoded):
+    (exponent, significand) = encoded
+    return (exponent << 14) | significand
+
 def decode(x):
     (exponent, significand) = x
     return significand * 2**exponent
 
-for num in [64, 16389, 54823, 96737, 124832, 113877]:
+for num in [16, 235, 1987, 6874, 27543, 75654, 99991]:
     print(num)
     encoded = encode(num)
     print(encoded)
+    print(encoded_as_int(encoded))
+    print(f"{encoded_as_int(encoded):b}")
     print(decode(encoded))
     print()
 
