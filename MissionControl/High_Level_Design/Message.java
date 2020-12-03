1d130e7c41dd5ceea1e4f45ce9174db44759e919
Sabrina Contaldi <sabrinacontaldi@Sabrinas-MacBook-Air.local>
2020-12-03 11:30:32 +0200

    Message class

---
+++ MissionControl/High_Level_Design/Message.java
@@ -0,0 +1,17 @@
+package MissionControl.High_Level_Design;
+
+public class Message 
+{
+
+    
+    public static class Driver
+    {
+        public static void main(String[] args) {
+            
+            System.out.println(Field.Latitude.encode(43.00));
+
+
+        }
+    }
+
+}
