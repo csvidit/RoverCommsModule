61f7cbd3b3e8584139c9419ba96f886bd87dd285
Vidit Khandelwal <63170797+csvidit@users.noreply.github.com>
2020-12-10 00:36:31 +0530

    changed code for accepting input

--- MissionControl/High_Level_Design/Message.java
+++ MissionControl/High_Level_Design/Message.java
@@ -1,5 +1,11 @@
 package MissionControl.High_Level_Design;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.io.File;
+import java.nio.file.Path;
+
 public class Message 
 {
     public static class Driver
@@ -10,16 +16,49 @@ public class Message
             
             //input the values
             
-            double latitude = 90;
-            double longitude = 180;
-            double chargeLevel = 100;
-            int waitCondition = 92;
-            int missionMode = 4;
-            //int lightingLevel;
-            double ambientTemp = -28.5;
-            double internalTemp = -0.5;
-            double windSpeed = 182.5;
-            long time = 1589656753210L;
+            Path messageInput = new Path("/");
+            
+            /**If reading from standard input, use the code below */
+            Scanner sc = new Scanner(System.in);
+
+            /**If reading from file, use the code below */
+            //Scanner sc = new Scanner(File fileName);
+
+            double latitude, longitude, chargeLevel, ambientTemp, internalTemp, windSpeed;
+            int waitCondition, missionMode, lightingLevel;
+            long time;
+
+            System.out.print("\nLatitude: ");
+            latitude=sc.nextDouble();
+            System.out.print("\nLongitude: ");
+            longitude=sc.nextDouble();
+            System.out.print("\nCharge Level: ");
+            chargeLevel=sc.nextDouble();
+            System.out.print("\nWait Condition: ");
+            waitCondition=sc.nextInt();
+            System.out.print("\nMission Mode: ");
+            missionMode=sc.nextInt();
+            System.out.print("\nLighting Level: ");
+            lightingLevel=sc.nextInt();
+            System.out.print("\nAmbient Temperature: ");
+            ambientTemp=sc.nextDouble();
+            System.out.print("\nInternal Temperature: ");
+            internalTemp=sc.nextDouble();
+            System.out.print("\nWind Speed: ");
+            windSpeed=sc.nextDouble();
+            System.out.print("\nTime: ");
+            time=sc.nextLong();
+
+            /*latitude = 90;
+            longitude = 180;
+            chargeLevel = 100;
+            waitCondition = 92;
+            missionMode = 4;
+            //lightingLevel = 
+            ambientTemp = -28.5;
+            internalTemp = -0.5;
+            windSpeed = 182.5;
+            time = 1589656753210L;*/
 
 
             //System.out.println(Field.Latitude.encode(-7.34));
